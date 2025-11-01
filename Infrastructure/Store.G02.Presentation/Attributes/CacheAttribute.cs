1bfb839d663bb29ce229e190cfcb3a5da04b51bb
c 2 d <c 2 d@DESKTOP-C9UNDFV>
2025-11-01 13:59:19 +0200

    APIS05

---
+++ Infrastructure/Store.G02.Presentation/Attributes/CacheAttribute.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
+using Store.G02.Services.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.G02.Presentation.Attributes
+{
+    public class CacheAttribute(int durationInSec) : Attribute, IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var cacheService = context.HttpContext.RequestServices.GetRequiredService<IServiceManager>().CacheService;
+
+            var cacheKey = GenerateCacheKey(context.HttpContext.Request);
+
+            var result = await cacheService.GetCacheValueAsync(cacheKey);
+
+            if(!string.IsNullOrEmpty(result))
+            {
+                // Return Response
+                context.Result = new ContentResult()
+                { 
+                    ContentType = "application/json",
+                    StatusCode = StatusCodes.Status200OK,
+                    Content = result
+                };
+                return;
+            }
+
+            // Execute The Endpoint
+            var contextResult = await next.Invoke();
+            if (contextResult.Result is OkObjectResult okObject)
+            {
+                await cacheService.SetCacheValueAsync(cacheKey, okObject.Value, TimeSpan.FromSeconds(durationInSec));
+            }
+
+        }
+    
+        
+        private string GenerateCacheKey(HttpRequest request)
+        {
+            var key = new StringBuilder();
+            key.Append(request.Path);
+
+            foreach (var item in request.Query.OrderBy(q => q.Key))
+            {
+                key.Append($"|{item.Key}-{item.Value}");
+            }
+
+
+            return key.ToString();
+        }
+    
+    }
+}
