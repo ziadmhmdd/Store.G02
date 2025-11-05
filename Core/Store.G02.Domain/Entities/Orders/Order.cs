a94eed3ce741341a9841476fdd35d485f958ef53
c 2 d <c 2 d@DESKTOP-C9UNDFV>
2025-11-05 23:57:59 +0200

    APIS07

---
+++ Core/Store.G02.Domain/Entities/Orders/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.G02.Domain.Entities.Orders
+{
+    public class Order : BaseEntity<Guid>
+    {
+        public string UserEmail { get; set; }
+        public DateTimeOffset OrderDate { get; set; } = DateTimeOffset.Now;
+        public OrderStatus Status { get; set; }
+    }
+}
